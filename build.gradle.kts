61bb353386b8669f43939640d0f97fd9788c8927
James Ward <james@jamesward.com>
2025-11-13 08:39:43 -0700

    bump spring ai

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ subprojects {
 
     configure<io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension> {
         imports {
-            mavenBom("org.springframework.ai:spring-ai-bom:1.1.0-RC1")
+            mavenBom("org.springframework.ai:spring-ai-bom:1.1.0")
         }
     }
 }
